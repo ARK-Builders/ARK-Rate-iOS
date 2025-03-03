5f3c10bdea9bbeda3e29480ad14bfbdd761e485e
phuhk <laughingintheway@gmail.com>
2025-03-03 20:05:01 +0700

    Create Xcode project

---
+++ ARK Rate/ARK_RateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARK_RateApp.swift
+//  ARK Rate
+//
+//  Created by Huỳnh Kỳ Phú on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ARK_RateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
