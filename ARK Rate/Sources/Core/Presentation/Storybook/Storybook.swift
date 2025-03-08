f11290e123594c6c7dce3197d3f11b60aa1d4ddb
phuhk <laughingintheway@gmail.com>
2025-03-09 00:03:17 +0700

    Add Storybook to preview Components

---
+++ ARK Rate/Sources/Core/Presentation/Storybook/Storybook.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct Storybook: PreviewProvider {
+
+    static var previews: some View {
+        VStack {
+            Text("CurrencyRowView")
+            CurrencyRowView(currencyName: "USD", currencyRate: "1.00")
+            CurrencyRowView(currencyName: "VND", currencyRate: "25515.16")
+            CurrencyRowView(currencyName: "KRW", currencyRate: "1446.19")
+        }
+    }
+}
