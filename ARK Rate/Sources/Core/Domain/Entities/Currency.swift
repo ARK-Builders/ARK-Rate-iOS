8fb588cd3fc5707f2b92cddf93beb420cb6e81bd
phuhk <laughingintheway@gmail.com>
2025-03-24 20:41:57 +0700

    Add strings

--- ARK Rate/Sources/Core/Domain/Entities/Currency.swift
+++ ARK Rate/Sources/Core/Domain/Entities/Currency.swift
@@ -12,4 +12,8 @@ struct Currency {
     let code: String
     let rate: Decimal
     let category: Category
+
+    var name: String {
+        String(localized: String.LocalizationValue(code))
+    }
 }
